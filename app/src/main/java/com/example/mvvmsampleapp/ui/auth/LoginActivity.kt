92f26ae0c42c42fede8973f634a57d14b6c80f38
Rekafekili <rekafekili@gmail.com>
2020-05-21 07:43:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mvvmsampleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.example.mvvmsampleapp.R
+import com.example.mvvmsampleapp.databinding.ActivityLoginBinding
+import com.example.mvvmsampleapp.util.toast
+
+class LoginActivity : AppCompatActivity(), AuthListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+    }
+
+    override fun onStarted() {
+        toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("Login Success")
+    }
+
+    override fun onFailure(message: String) {
+        toast(message)
+    }
+}
